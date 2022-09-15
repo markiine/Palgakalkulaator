46fa853ff1bb9c6bda567241de7b8380b4ac3a7a
Opilane TTHK <opilane@tthk.ee>
2022-09-15 14:06:32 +0300

    SalaryCalculator

---
+++ Palgakalkulaator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Palgakalkulaator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // kood 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//Palga kalkulaator Test 
+//1. Sina sisestad brutotasu ja valem arvutab välja netotasu.
+//2. Kokku peab olema kolm erinevat meetodit. Enne kui meetod algab, siis peab olema kontroll, et millist meetodi hakatakse kasutama ja sisestatud palgasumma peab minema edasi järgmisse meetodi.
+//3. Netotasu peab arvutama selliselt välja, et sina ei peaks tuludeklaratsiooni ajal juurde maksma ega saa pärast riigilt raha juurde kuna oled rohkem maksnud.
+//4. Kood peab olema inglisekeelne.
+//Palgakalkulaator:
+//https://www.kalkulaator.ee/et/palgakalkulaator
+//Palga arvutamise valem:
+//https://www.emta.ee/et/maksuvaba-tulu
+
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ref
+
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/out-parameter-modifier
+
+// https://www.c-sharpcorner.com/UploadFile/ff2f08/ref-vs-out-keywords-in-C-Sharp/
